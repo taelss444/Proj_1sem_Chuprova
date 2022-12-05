3c1196c28046cdb4419feea6e5772413cdc181d2
taels <taelss4liza@gmail.com>
2022-12-05 23:08:42 +0300

    PZ_10

---
+++ PZ_10/PZ_10.1.py
@@ -0,0 +1,3 @@
+voyage = {"Мексика", "Канада", "Израиль", "Италия"}
+ReynaTur = {"Англия", "Япония", "Канада", "ЮАР"}
+print(voyage - ReynaTur,"\n", ReynaTur - voyage,"\n", voyage & ReynaTur,"\n", voyage == ReynaTur)
