46f0e2c1fa67dfff16ff6fd572bd6837b1581abf
тэли <113168684+taelss444@users.noreply.github.com>
2023-01-29 20:23:02 +0300

    no

--- PZ_12/PZ_12.2.py
+++
@@ -1,7 +0,0 @@
-# 2.Составить генератор (yield), который выводит из строки только цифры.
-
-def digiter(st: str):
-    yield "".join(c for c in st if c.isdigit())
-
-
-print(f'Ваши цифры : {next(digiter(input("Введите строку : ")))}')
